336f15ca1d6bb9f4ea1e3662fc187e9eaa9eaf5a
Stephen L. McMahon <smcmahon@slb.com>
2023-12-17 09:19:00 -0500

    Updated to use shared library

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module slmcmahon.com/varlist
 go 1.21.5
 
 require (
+	github.com/olekukonko/tablewriter v0.0.5
+	github.com/slmcmahon/go-common v0.0.0-20231217131042-f828f497f218
+)
+
+require (
 	github.com/mattn/go-runewidth v0.0.9 // indirect
-	github.com/olekukonko/tablewriter v0.0.5 // indirect
-	github.com/slmcmahon/go-common v0.0.0-20231217131042-f828f497f218 // indirect
+	github.com/slmcmahon/go-azdo v0.0.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,31 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
 	"sort"
 
 	"github.com/olekukonko/tablewriter"
+	"github.com/slmcmahon/go-azdo"
 	slmcommon "github.com/slmcmahon/go-common"
 )
 
-type VarLib struct {
-	Name string `json:"name"`
-	Id   int    `json:"id"`
-}
-
-type VarLibResponse struct {
-	Count   int      `json:"count"`
-	VarLibs []VarLib `json:"value"`
-}
-
 // ByName implements sort.Interface based on the Name field.
-type ByName []VarLib
+type ByName []azdo.VarLib
 
 func (a ByName) Len() int           { return len(a) }
 func (a ByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
@@ -58,39 +46,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	url := fmt.Sprintf("https://dev.azure.com/%s/%s/_apis/distributedtask/variablegroups?api-version=6.0-preview.2", org, project)
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
+	response, err := azdo.GetVariableLibraries(pat, org, project)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	req.SetBasicAuth("", pat)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	var response VarLibResponse
-	err = json.Unmarshal([]byte(body), &response)
-	if err != nil {
-		panic(err)
-	}
-
-	sort.Sort(ByName(response.VarLibs))
+	sort.Sort(ByName(response.Value))
 
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Name", "ID"})
 
-	for _, varlib := range response.VarLibs {
-		table.Append([]string{varlib.Name, fmt.Sprintf("%d", varlib.Id)})
+	for _, varlib := range response.Value {
+		table.Append([]string{varlib.Name, fmt.Sprintf("%d", varlib.ID)})
 	}
 
 	table.Render()
